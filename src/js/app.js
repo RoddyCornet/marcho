bd9803d406bac5c5744d160d945fa32c801aa435
RoddyCornet <94225985+RoddyCornet@users.noreply.github.com>
2022-02-16 18:10:53 +0300

    added product

--- src/js/app.js
+++ src/js/app.js
@@ -13,10 +13,15 @@ $(function () {
 
 	$('.top-slider__inner').slick({
 		dots: true,
-		arrows:false,
-		fade:true,
-		autoplay:true,
-		autoplaySpead:2000
+		arrows: false,
+		fade: true,
+		autoplay: true,
+		autoplaySpead: 2000
 	});
-
+	$(".star").rateYo({
+		starWidth: "17px",
+		normalFill: "#ccccce",
+		ratedFill: "#ffc35b",
+		readOnly: true
+	});
 });
